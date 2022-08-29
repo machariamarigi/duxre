00e0e1a5f3042677d04b5fa168de57187bc2737f
machariamarigi <machariamarigi@gmail.com>
2022-08-29 23:52:38 +0300

    create pokemon slice and add it to the store

---
+++ store/slices/pokemonSlice.ts
@@ -0,0 +1,74 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+interface Pokemon {
+  id: number;
+  name: string;
+  image: string;
+}
+
+export type PokemonState = {
+  pokemon: Pokemon[];
+  search: string;
+  filteredPokemon: Pokemon[];
+  pending: boolean;
+  error: boolean;
+};
+
+const initialState: PokemonState = {
+  pokemon: [],
+  filteredPokemon: [],
+  search: "",
+  pending: false,
+  error: false,
+};
+
+export const getPokemon = createAsyncThunk("pokemon/getPokemon", async () => {
+  const response = await await fetch(
+    "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
+  );
+
+  return await response.json();
+});
+
+export const pokemonSlice = createSlice({
+  name: "pokemon",
+  initialState,
+  reducers: {
+    rehydrate(state, action: PayloadAction<PokemonState>) {
+      state.error = action.payload.error;
+      state.pending = action.payload.pending;
+      state.pokemon = action.payload.pokemon;
+      state.filteredPokemon = action.payload.filteredPokemon;
+      state.search = action.payload.search;
+    },
+    setSearch(state, action: PayloadAction<string>) {
+      state.search = action.payload;
+      state.filteredPokemon = state.pokemon.filter(({ name }) => {
+        name.toLowerCase().includes(state.search.toLowerCase());
+      });
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(getPokemon.pending, (state) => {
+      state.pending = true;
+    });
+    builder.addCase(getPokemon.fulfilled, (state, { payload }) => {
+      state.pending = false;
+      state.pokemon = payload;
+      state.filteredPokemon = payload;
+    });
+    builder.addCase(getPokemon.rejected, (state) => {
+      state.pending = false;
+      state.error = true;
+    });
+  },
+});
+
+export const { rehydrate, setSearch } = pokemonSlice.actions;
+
+export const selectSearch = (state: RootState) => state.pokemon.search;
+export const selectFilteredPokemon = (state: RootState) =>
+  state.pokemon.filteredPokemon;
+
+export default pokemonSlice.reducer;
--- store/store.ts
+++ store/store.ts
@@ -1,12 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import counterReducer from "./slices/counterSlice";
+import pokemonReducer from "./slices/pokemonSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    pokemon: pokemonReducer,
   },
 });
 
 // Infer rhe `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-export type Appdispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
